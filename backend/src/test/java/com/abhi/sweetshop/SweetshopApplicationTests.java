c5bc3f973c225b9c154cdaa92ba997b9a9058517
AbhinayYendoti <abhiyendoti@gmail.com>
2025-12-12 22:12:17 +0530

    chore: initial project setup

---
+++ backend/src/test/java/com/abhi/sweetshop/SweetshopApplicationTests.java
@@ -0,0 +1,12 @@
+package com.abhi.sweetshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SweetshopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
